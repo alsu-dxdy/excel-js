ba1e716fd995eb661100e618d184288c7f3f65ba
Alsu Abdrahmanova <aabdrahmanova@nvtech.ru>
2021-06-26 21:18:49 +0300

    add @babel/plugin-proposal-class-properties

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,8 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 
 export class Table extends ExcelComponent {
+    static className = 'excel__table'
+
     toHTML() {
         return '<h1>33</h1>'
     }
